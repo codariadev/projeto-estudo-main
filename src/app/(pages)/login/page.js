6e93c3e7fd02ccfe4e58775cdd7521c4708e15a7
codariadev <alves2.eduardo@gmail.com>
2026-07-08 21:03:09 -0300

    ##Ajuste de exemplos, import e uso do css, main e comentario.

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -2,6 +2,8 @@ import styles from './page.module.css'
 
 export default function Login() {
   return (
-  //Codigo HTML
-  )
+    <main className={styles.container}>
+      {/* // Codigo HTML */}
+    </main>
+  );
 }

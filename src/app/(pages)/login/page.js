9e0e1079682330f9a6e576c9beccd29c7d40d01d
Wilson Jr <106275510+cientistajunior@users.noreply.github.com>
2026-07-17 19:53:11 +0000

    fix: refactory class and config css

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -4,19 +4,19 @@ export default function Login() {
   return (
     <main className={styles.loginPage}>  
             <form className={styles.loginCard}>
-              <img src="" alt="logo"/>
+              <img src="/logo.png" alt="logo"/>
                 <div className={styles.loginForm}>
-                    <label for="email" className={styles.labelForm}>Email</label>
-                    <input type="email" name="email" id="email" required/>
+                  
+                    <input type="email" name="email" id="email" placeholder='Email' required/>
                 
-                    <label for="password" className={styles.labelForm}>Password</label>
-                    <input type="password" name="password" id="password"/>
+                    <input type="password" name="password" id="password" placeholder='Senha'/>
 
                     {/* <button className={styles.submit} type="submit">Log in</button> */}
-                    <p className={styles.p}><a href="#" target="_blank">Forgot password?</a></p>
+                    <p className={styles.forgotPassword}><a href="#" target="_blank">Esqueceu sua senha?</a></p>
                 </div>
-                <p className={styles.p}>Don't have an account? <a href="#" target="_blank">Sign up</a></p>
+                <p className={styles.noAccount}>Ainda não tenho conta?<a href="#" target="_blank">inscrever-se</a></p>
             </form>
     </main>
   );
 }
+

5807bae5af49fbb7a6e84a1a7c18528cd8864f3a
Wilson Neves Moreira Junior <wilson.moreira@tse.jus.br>
2026-07-17 13:05:54 -0300

    altenado para codespace

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -2,23 +2,21 @@ import styles from './page.module.css'
 
 export default function Login() {
   return (
-    <main className={styles.container}>
-        
-            <h1>Login</h1>  
-            <form className={styles.form}>
-                <div className={styles.divForm}>
+    <main className={styles.loginPage}>  
+            <form className={styles.loginCard}>
+              <img src="" alt="logo"/>
+                <div className={styles.loginForm}>
                     <label for="email" className={styles.labelForm}>Email</label>
                     <input type="email" name="email" id="email" required/>
-                </div>
-                <div className={styles.divForm}>
+                
                     <label for="password" className={styles.labelForm}>Password</label>
                     <input type="password" name="password" id="password"/>
+
+                    {/* <button className={styles.submit} type="submit">Log in</button> */}
+                    <p className={styles.p}><a href="#" target="_blank">Forgot password?</a></p>
                 </div>
-                <p className={styles.p}><a href="#" target="_blank">Forgot password?</a></p>
-                <button className={styles.submit} type="submit">Log in</button>
                 <p className={styles.p}>Don't have an account? <a href="#" target="_blank">Sign up</a></p>
             </form>
-        
     </main>
   );
 }

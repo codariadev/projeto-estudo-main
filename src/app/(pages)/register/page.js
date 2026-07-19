e01d764d549d283fe25aa9cdff67c48082ff0e2c
codariadev <alves2.eduardo@gmail.com>
2026-07-19 20:00:53 -0300

    feat/Fix: Adicionado componente botão primario e organizado css das paginas existentes

--- src/app/(pages)/register/page.js
+++ src/app/(pages)/register/page.js
@@ -1,20 +1,38 @@
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Button from "@/components/primaryButton/primaryButton";
 
 export default function Register() {
   return (
     <main className={styles.registerPage}>
-            <form className={styles.registerCard}>
-              <img src="/logo.png" alt="logo"/>
-                <div className={styles.registerForm}>      
-                    <input type="email" name="email" id="email" placeholder='Email'/>
-                
-                    <input type="password" name="password" id="password" placeholder='Senha'/>
+      <form className={styles.registerCard}>
+        <Image src="/logo.png" alt="logo" width={405} height={270} loading="eager"/>
+        <div className={styles.registerForm}>
+          <input type="email" name="email" id="email" placeholder="Email" />
 
-                    <input type="password" name="password" id="password" placeholder='Confirme sua senha'/>
-                    {/* <button className={styles.submit} type="submit">Log in</button> */}
-                </div>
-              <p className={styles.haveAccount}>Já tem conta? <a href="#" target="_blank">acesse</a></p>
-            </form>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Senha"
+          />
+
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Confirme sua senha"
+          />
+
+          <Button variant="primary">Registrar</Button>
+        </div>
+        <p className={styles.haveAccount}>
+          Já tem conta?{" "}
+          <a href="#" target="_blank">
+            acesse
+          </a>
+        </p>
+      </form>
     </main>
   );
 }

233b76c519cfddd4b2879dfb0e4f0dc13e020934
Wilson Jr <106275510+cientistajunior@users.noreply.github.com>
2026-07-23 16:31:41 +0000

    feat: seção topbar

--- src/components/topbar/page.js
+++ src/components/topbar/page.js
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+import Button from '../primaryButton/primaryButton';
 import styles from './page.module.css';
 
 export default function Topbar() {
   return (
     <header className={styles.topbar}>
-      TOPBAR
+      <div className={styles.searchContainer}>
+        <input placeholder='Procurar'/>
+        <Button variant="search">Pesquisar</Button>
+      </div>
+      <div className={styles.rightIcon}>
+        <Image src="/next.svg" alt='notifcação' width={25} height={25}/>
+        <Image src="/next.svg" alt='email' width={25} height={25}/>
+        <Image src="/next.svg" alt='perfil' width={25} height={25}/>
+      </div>
     </header>
   );
 }

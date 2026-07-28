54e0f798e357b3c240f148a10875fd7d789650fd
Wilson Jr <106275510+cientistajunior@users.noreply.github.com>
2026-07-28 16:41:38 +0000

    feat/fix: home css

--- src/app/(system)/home/page.js
+++ src/app/(system)/home/page.js
@@ -13,18 +13,18 @@ export default function HomePage() {
       <div className={styles.cardIcon}>
         <div className={styles.cardSection}>
           <Image className={styles.ImageMala} src="/icon-dashboard-card-section/card-mala.svg" alt="logo" width={75} height={75} loading="eager" />
-          <p>Projetos Ativos</p>
-          <p>12</p>
-          <p><span>&uarr;20%</span> em relação ao mês anterior.</p>
+          <p className={styles.text}>Projetos Ativos</p>
+          <p className={styles.num}>12</p>
+          <p className={styles.info}><span>&uarr;20%</span> em relação ao mês anterior.</p>
         </div>
         <div className={styles.cardSection}>
-          <Image className={styles.ImageMala} src="/icon-dashboard-card-section/relogio-parede.svg" alt="Mala de viagem" width={75} height={75} loading="eager" />
+          <Image className={styles.ImageRelogioParede} src="/icon-dashboard-card-section/relogio-parede.svg" alt="Mala de viagem" width={75} height={75} loading="eager" />
           <p>Projetos Ativos</p>
           <p>12</p>
           <p><span>&uarr;12%</span> em relação ao mês anterior.</p>
         </div>
         <div className={styles.cardSection}>
-          <Image className={styles.ImageMala} src="/icon-dashboard-card-section/projetos-ativos.svg" alt="relogio" width={75} height={75} loading="eager" />
+          <Image className={styles.ImageProjetosAtivos} src="/icon-dashboard-card-section/projetos-ativos.svg" alt="relogio" width={75} height={75} loading="eager" />
           <p>Projetos Ativos</p>
           <p>12</p>
           <p><span>&uarr;5%</span> em relação ao mês anterior.</p>
